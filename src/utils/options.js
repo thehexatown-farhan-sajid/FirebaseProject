60c12aafc794a22e6a9a14adb95f21d1a91addea
thehexatown-farhan-sajid <116056518+thehexatown-farhan-sajid@users.noreply.github.com>
2023-01-12 11:48:37 +0500

    Goerli

--- src/utils/options.js
+++ src/utils/options.js
@@ -1,4 +1,4 @@
-export const hexanftAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
-export const hexaMarketplaceAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
-export const hexanAuctionAddress = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"
-export const hexa1155Address = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9"
+export const hexanftAddress = "0xcC90E2654084B88F4CdeD99903018B199B1D6729"
+export const hexaMarketplaceAddress = "0x68d017d251a6080B39a2745369ef148702828906"
+export const hexanAuctionAddress = "0x5431bFf9E732Cd28c1500e3ec24cdF5F6a0b804A"
+export const hexa1155Address = "0x115249D425B2A74073b961446fDB197cE1bF2672"
